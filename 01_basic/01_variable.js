c61b7a61b9b1085a96c73fb051670e821ab57661
Ritik raja <110325525+Ritikraja07@users.noreply.github.com>
2024-04-17 18:38:47 +0000

    01_variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,25 @@
+console.log("Ritik raja");
+
+const accountId = 144553;
+let accountEmail = "ritik@google.com";
+var accountPassword = "12345";
+
+accountCity = "Durgapur";
+let accountState;
+
+// accountId = 2; 
+//const variable dont allow to change any data to change
+accountEmail = "rr@rr.com";
+accountPassword = "212121";
+accountCity = "Patna"; //don't use this type to declare variable
+
+console.table([accountId, accountEmail
+, accountPassword,accountCity,accountState]);
+
+{
+    // scope
+}
+
+/* 
+prefer not to use var because of issue in block scope and functional scope 
+*/
